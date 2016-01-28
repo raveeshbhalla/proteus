4ee11f41f928487c6e409326c08e22c1ecc47bb7
Raveesh Bhalla <raveeshbhalla.90@gmail.com>
2016-01-28 16:06:36 +0530

    Added tint to button

--- demo/src/main/java/in/raveesh/proteusdemo/MainActivity.java
+++ demo/src/main/java/in/raveesh/proteusdemo/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 
 import java.util.Random;
 
+import in.raveesh.proteus.Button;
 import in.raveesh.proteus.ImageView;
 
 
@@ -17,35 +18,35 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ImageView imageView = (ImageView)findViewById(R.id.imageView);
-        final TextView textView = (TextView)findViewById(R.id.textView);
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+        final ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        final TextView textView = (TextView) findViewById(R.id.textView);
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Random random = new Random();
                 int s = random.nextInt(4 - 0);
+                int color;
                 switch (s) {
                     case 0:
-                        imageView.setPaintedDrawableFromResource(R.drawable.ic_action_thumbs_up,
-                                android.R.color.holo_blue_bright);
+                        color = android.R.color.holo_blue_bright;
                         textView.setText("Blue");
                         break;
                     case 1:
-                        imageView.setPaintedDrawableFromResource(R.drawable.ic_action_thumbs_up,
-                                android.R.color.holo_green_light);
+                        color = android.R.color.holo_green_light;
                         textView.setText("Green");
                         break;
                     case 2:
-                        imageView.setPaintedDrawableFromResource(R.drawable.ic_action_thumbs_up,
-                                android.R.color.holo_orange_light);
+                        color = android.R.color.holo_orange_light;
                         textView.setText("Orange");
                         break;
-                    case 3:
-                        imageView.setPaintedDrawableFromResource(R.drawable.ic_action_thumbs_up,
-                                android.R.color.holo_purple);
+                    default:
+                        color = android.R.color.holo_purple;
                         textView.setText("Purple");
                         break;
                 }
+                imageView.setPaintedDrawableFromResource(R.drawable.ic_action_thumbs_up, color);
+                button.setPaintResource(color);
             }
         });
     }
